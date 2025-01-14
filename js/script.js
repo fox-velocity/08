108b615c7122ff9cc59cad326c5d182610d4fd82
FoxVelocity <foxvelocity@laposte.net>
2025-01-14 22:59:17 +0100

    logo64.js

--- js/script.js
+++ js/script.js
@@ -49,7 +49,7 @@ window.onload = function () {
         pdfMake = window.pdfMake;
          console.log("pdfMake is ready :", pdfMake)
        };
-   fetch('./logoBase64.txt')
+   fetch('./logoBase64.js')
         .then(response => response.text())
         .then(data => {
             logoBase64 = data;
