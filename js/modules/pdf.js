f7be854540b063a8d2eee9ae333cd9834c5ca5d4
VALLY <foxvelocity@laposte.net>
2025-01-15 17:28:04 +0100

    ordre bas de page

--- js/modules/pdf.js
+++ js/modules/pdf.js
@@ -72,18 +72,18 @@
         footer: function(currentPage, pageCount) {
             return {
                 stack: [
-                 
-                   {
-                      image: logoBase64,
-                       width: 50,
-                       alignment: 'center'
-                     },
                    {
                       text: 'Fox Velocity',
                       alignment: 'center',
                       fontSize: 8,
                       margin: [0, 0, 0, 0]
                    },
+                   {
+                      image: logoBase64,
+                       width: 50,
+                       alignment: 'center'
+                     },
+                 
                    {
                          text: `Page ${currentPage.toString()} sur ${pageCount}`,
                          alignment: 'right',
