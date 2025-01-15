258f7077c34c90925629ab06a718c23b05dff482
VALLY <foxvelocity@laposte.net>
2025-01-15 19:17:49 +0100

    Update theme.js

--- js/modules/theme.js
+++ js/modules/theme.js
@@ -1,39 +1,19 @@
 // theme.js
 
 export function initializeTheme() {
-    const body = document.body;
-    const sunIcon = document.querySelector('.sun-icon');
-    const moonIcon = document.querySelector('.moon-icon');
-    
-    // Vérifie si un thème est déjà enregistré dans localStorage
-    const savedTheme = localStorage.getItem('theme');
-    if (savedTheme === 'dark') {
-        body.removeAttribute('data-theme'); // Le thème sombre n'utilise pas d'attribut data-theme
-        sunIcon.style.display = 'none';
-        moonIcon.style.display = 'block';
-    } else {
-        body.setAttribute('data-theme', 'light');
-        sunIcon.style.display = 'block';
-        moonIcon.style.display = 'none';
-    }
+     const body = document.body;
+     body.setAttribute('data-theme', 'light');
+     const sunIcon = document.querySelector('.sun-icon');
+     const moonIcon = document.querySelector('.moon-icon');
+      sunIcon.style.display = 'block';
+       moonIcon.style.display = 'none';
 }
 
 export function toggleTheme() {
     const body = document.body;
     const sunIcon = document.querySelector('.sun-icon');
     const moonIcon = document.querySelector('.moon-icon');
-    const currentTheme = body.getAttribute('data-theme');
-
-    if (currentTheme === 'light') {
-        body.removeAttribute('data-theme');
-        localStorage.setItem('theme', 'dark');
-        sunIcon.style.display = 'none';
-        moonIcon.style.display = 'block';
-    } else {
-        body.setAttribute('data-theme', 'light');
-        localStorage.setItem('theme', 'light');
-        sunIcon.style.display = 'block';
-        moonIcon.style.display = 'none';
-    }
-}
-window.toggleTheme = toggleTheme;
+     sunIcon.style.display = sunIcon.style.display === 'none' ? 'block' : 'none';
+   moonIcon.style.display = moonIcon.style.display === 'none' ? 'block' : 'none';
+  }
+   window.toggleTheme = toggleTheme;
