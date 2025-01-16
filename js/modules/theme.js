f99ecf18d7911bd0bc70a0eac51786ac9551a29a
VALLY <foxvelocity@laposte.net>
2025-01-16 15:37:35 +0100

    Update theme.js

--- js/modules/theme.js
+++ js/modules/theme.js
@@ -1,19 +1,18 @@
 // theme.js
 
 export function initializeTheme() {
-     const body = document.body;
-     body.setAttribute('data-theme', 'light');
-     const sunIcon = document.querySelector('.sun-icon');
-     const moonIcon = document.querySelector('.moon-icon');
-      sunIcon.style.display = 'block';
-       moonIcon.style.display = 'none';
+    const body = document.body;
+    body.setAttribute('data-theme', 'light');
 }
 
 export function toggleTheme() {
     const body = document.body;
-    const sunIcon = document.querySelector('.sun-icon');
+    const currentTheme = body.getAttribute('data-theme');
+    const newTheme = currentTheme === 'light' ? 'dark' : 'light';
+    body.setAttribute('data-theme', newTheme);
+
+     const sunIcon = document.querySelector('.sun-icon');
     const moonIcon = document.querySelector('.moon-icon');
      sunIcon.style.display = sunIcon.style.display === 'none' ? 'block' : 'none';
    moonIcon.style.display = moonIcon.style.display === 'none' ? 'block' : 'none';
-  }
-   window.toggleTheme = toggleTheme;
+}
