0b6efff3dfde42267841e85cf1d2f6ebfb9c9250
VALLY <foxvelocity@laposte.net>
2025-01-16 16:14:57 +0100

    liste des symbole

--- js/modules/constants.js
+++ js/modules/constants.js
@@ -2,21 +2,52 @@
 
 // Définition des symboles de devises
 export const currencySymbols = {
-    'USD': '$',
+     'USD': '$',
     'CNY': 'CN¥',
     'CAD': 'C$',
     'AUD': 'A$',
-    'INR': 'R',
+    'INR': '₹',
     'EUR': '€',
     'GBP': '£',
     'JPY': '¥',
     'HKD': 'HK$',
+    'MXN': 'MX$',
+    'BRL': 'R$',
+    'ZAR': 'R',
+    'SGD': 'S$',
+    'KRW': '₩',
+    'TWD': 'NT$',
+    'CHF': 'CHF',
+    'NZD': 'NZ$',
+    'SAR': 'SR',
+    'IDR': 'Rp',
+    'TRY': '₺',
+    'EGP': 'E£',
+    'PHP': '₱',
+    'THB': '฿',
+    'VND': '₫',
+    'LKR': 'Rs',
+    'BDT': '৳',
+    'ARS': '$',
+    'JMD': 'J$',
+    'GHS': 'GH₵',
+    'NGN': '₦',
+    'UGX': 'USh',
+    'MUR': 'Rs',
+    'RON': 'lei',
+    'RSD': 'дин.',
+    'ALL': 'L',
+    'CZK': 'Kč',
+    'PLN': 'zł',
+    'HUF': 'Ft',
+    'RUB': '₽',
 };
 
 // Conversion des places boursières en devises
 export const exchangeToCurrency = {
-    'NASDAQ': 'USD',
+        'NASDAQ': 'USD',
     'NYSE': 'USD',
+    'NYM': 'USD',
     'AMEX': 'USD',
     'NYQ': 'USD',
     'PCX': 'USD',
@@ -34,5 +65,56 @@ export const exchangeToCurrency = {
     'LIS': 'EUR',
     'DUB': 'EUR',
     'LSE': 'GBP',
-    'AIM': 'GBP'
+    'AIM': 'GBP',
+    'TSE': 'JPY', // Tokyo Stock Exchange
+    'HKEX': 'HKD', // Hong Kong Stock Exchange
+    'SSE': 'CNY', // Shanghai Stock Exchange
+    'SZSE': 'CNY', // Shenzhen Stock Exchange
+    'BSE': 'INR', // Bombay Stock Exchange
+    'NSE': 'INR', // National Stock Exchange of India
+    'ASX': 'AUD', // Australian Securities Exchange
+    'TSX': 'CAD', // Toronto Stock Exchange
+    'BMV': 'MXN', // Bolsa Mexicana de Valores
+    'B3': 'BRL', // Brasil, Bolsa, Balcão
+    'JSE': 'ZAR', // Johannesburg Stock Exchange
+    'SGX': 'SGD', // Singapore Exchange
+    'KRX': 'KRW', // Korea Exchange
+    'TWSE': 'TWD', // Taiwan Stock Exchange
+    'SWX': 'CHF', // SIX Swiss Exchange
+    'NZX': 'NZD', // New Zealand Exchange
+    'OMX': 'EUR', // NASDAQ OMX Nordic Exchange
+    'SASE': 'SAR', // Saudi Stock Exchange
+    'IDX': 'IDR', // Indonesia Stock Exchange
+    'BIST': 'TRY', // Borsa Istanbul
+    'EGX': 'EGP', // Egyptian Exchange
+    'KSE': 'KRW', // Korea Stock Exchange
+    'TPEX': 'TWD', // Taipei Exchange
+    'BME': 'EUR', // Bolsas y Mercados Españoles
+    'EURONEXT': 'EUR', // Euronext
+    'MOEX': 'RUB', // Moscow Exchange
+    'WSE': 'PLN', // Warsaw Stock Exchange
+    'BUX': 'HUF', // Budapest Stock Exchange
+    'PSE': 'PHP', // Philippine Stock Exchange
+    'SET': 'THB', // Stock Exchange of Thailand
+    'HOSE': 'VND', // Ho Chi Minh Stock Exchange
+    'HNX': 'VND', // Hanoi Stock Exchange
+    'CSE': 'LKR', // Colombo Stock Exchange
+    'DSE': 'BDT', // Dhaka Stock Exchange
+    'CSE': 'CAD', // Canadian Securities Exchange
+    'NEO': 'CAD', // NEO Exchange
+    'ATHEX': 'EUR', // Athens Stock Exchange
+    'BCBA': 'ARS', // Buenos Aires Stock Exchange
+    'CSE': 'JMD', // Jamaica Stock Exchange
+    'GSE': 'GHS', // Ghana Stock Exchange
+    'NSE': 'NGN', // Nigerian Stock Exchange
+    'USE': 'UGX', // Uganda Securities Exchange
+    'CMA': 'MUR', // Stock Exchange of Mauritius
+    'CSE': 'MUR', // Stock Exchange of Mauritius
+    'BVB': 'RON', // Bucharest Stock Exchange
+    'BELEX': 'RSD', // Belgrade Stock Exchange
+    'LUSE': 'ALL', // Albania Stock Exchange
+    'CSE': 'CZK', // Prague Stock Exchange
+    'BET': 'RON', // Bucharest Stock Exchange
+    'BCS': 'RON', // Bucharest Stock Exchange
+    'CSE': 'CZK', // Prague Stock Exchang
 };
