990eac8587469726c75588138daf3409a4f7d093
VALLY <foxvelocity@laposte.net>
2025-01-29 17:29:56 +0100

    Update charts.js

--- js/modules/charts.js
+++ js/modules/charts.js
@@ -1,139 +1,3 @@
-// charts.js
-
-const { Chart } = window;
-
-let evolutionChart = null;
-let investmentChart = null;
-let savingsChart = null;
-
-// Fonction pour mettre à jour le graphique d'évolution
-export function updateEvolutionChart(labels, prices) {
-    const ctxEvolution = document.getElementById('evolutionChart').getContext('2d');
-    if (evolutionChart) evolutionChart.destroy();
-    // Graphique de l'évolution du support choisi
-     evolutionChart = new Chart(ctxEvolution, {
-       type: 'line',
-       data: {
-           labels: labels,
-           datasets: [
-               {
-                   label: 'Prix du support',
-                   data: prices,
-                   borderColor: 'rgb(0, 0, 255)',
-                   tension: 0.1,
-                   yAxisID: 'y',
-                   order: 1
-               }
-           ]
-       },
-       options: {
-          responsive: true,
-           maintainAspectRatio: false,
-           plugins: {
-               title: {
-                   display: true,
-                   text: 'Évolution du support choisi',
-                   font: {
-                       size: 25
-                   },
-                   color: 'black' // pour les caractères du titre
-               },
-               legend: {
-                   labels: {
-                       color: 'black' // Couleur noire pour les textes des légendes
-                   }
-               }
-           },
-           scales: {
-               y: {
-                   ticks: {
-                       color: 'black'
-                   }
-               },
-               x: {
-                   ticks: {
-                       color: 'black'
-                   }
-               }
-           }
-       }
-   });
-}
-
-// Fonction pour mettre à jour le graphique des montants investis et de l'évolution du portefeuille
-export function updateInvestmentChart(labels, investments, portfolio, portfolioValueEcreteAvecGain) {
-     const ctxInvestment = document.getElementById('investmentChart').getContext('2d');
-     if (investmentChart) investmentChart.destroy();
-     // Graphique des montants investis et de l'évolution du portefeuille
-     investmentChart = new Chart(ctxInvestment, {
-         type: 'bar',
-         data: {
-             labels: labels,
-             datasets: [
-                 {
-                     label: 'Valeur du portefeuille',
-                     data: portfolio,
-                     type: 'line',
-                     borderColor: 'rgb(255, 135, 0)',
-                     tension: 0.1,
-                     yAxisID: 'y',
-                     order: 1
-                 },
-                 {
-                     label: 'Valeur du portefeuille écrêté avec gains',
-                     data: portfolioValueEcreteAvecGain,
-                     type: 'line',
-                     borderColor: 'rgb(0, 0, 255)',
-                     tension: 0.1,
-                     yAxisID: 'y',
-                     order: 2
-                 },
-                 {
-                     label: 'Montant total investi',
-                     data: investments,
-                     backgroundColor: 'rgb(0, 255, 0)',
-                     borderColor: 'rgb(0, 255, 0)',
-                     borderWidth: 1,
-                     yAxisID: 'y',
-                     order: 3
-                 },
-             ]
-         },
-         options: {
-             responsive: true,
-             maintainAspectRatio: false,
-             plugins: {
-                 title: {
-                     display: true,
-                     text: 'Montants investis et évolution du portefeuille',
-                     font: {
-                         size: 25
-                     },
-                     color: 'black' // pour les caractères du titre
-                 },
-                 legend: {
-                     labels: {
-                         color: 'black' // Couleur noire pour les textes des légendes
-                     }
-                 }
-             },
-             scales: {
-                 y: {
-                     beginAtZero: true,
-                     ticks: {
-                         color: 'black'
-                     }
-                 },
-                 x: {
-                     ticks: {
-                         color: 'black'
-                     }
-                 }
-             }
-         }
-     });
-}
-
 export function updateSavingsChart(labels, investments, portfolio, monthlyInterestRate) {
     const ctxSavings = document.getElementById('savingsChart').getContext('2d');
     let cumulativeSavings = 0;
@@ -154,10 +18,13 @@ export function updateSavingsChart(labels, investments, portfolio, monthlyIntere
     lastCumulativeSavings = cumulativeSavings;
     const lastInvestment = investments[investments.length - 1]; // Récupérer le dernier investissement
      const gainTauxFixe = lastCumulativeSavings - lastInvestment; // Calculer le gain
+        console.log("cumulativeSavings du dernier mois :", lastCumulativeSavings);
+       console.log("investissement du dernier mois :", lastInvestment);
+       console.log("gain avec le taux fixe :", gainTauxFixe);
 
      if (savingsChart) savingsChart.destroy();
      savingsChart = new Chart(ctxSavings, {
-         type: 'bar',
+        type: 'bar',
          data: {
              labels: labels,
              datasets: [{
@@ -184,8 +51,8 @@ export function updateSavingsChart(labels, investments, portfolio, monthlyIntere
                     type: 'line',
                     borderColor: 'rgb(255, 135, 0)',
                     tension: 0.1,
-                     yAxisID: 'y',
-                     order : 1
+                    yAxisID: 'y',
+                    order : 1
                   }
              ]
          },
@@ -225,11 +92,5 @@ export function updateSavingsChart(labels, investments, portfolio, monthlyIntere
              }
          }
      });
-    console.log("cumulativeSavings du dernier mois :", lastCumulativeSavings);
-           console.log("investissement du dernier mois :", lastInvestment);
-           console.log("gain avec le taux fixe :", gainTauxFixe);
-    // return { lastCumulativeSavings, lastInvestment, gainTauxFixe }; // On retourne la nouvelle valeur
-}
-         }
-     });
+    // On ne retourne plus rien
 }
