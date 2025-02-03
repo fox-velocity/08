2cc519b329293ba23c9325ae3ca5eb3f5dc581f7
FoxVelocity <foxvelocity@laposte.net>
2025-02-03 15:00:37 +0100

    Update pdf.js

--- js/modules/pdf.js
+++ js/modules/pdf.js
@@ -1,4 +1,6 @@
-// pdf.js 23 12 31 01 mais le 01 02
+// pdf.js
+import { fontsBase64 } from './fonts.js';
+
 export async function generatePDF(pdfMake, logoBase64, logoRenardBase64Gris) {
     if (!pdfMake) {
         alert('pdfMake n\'est pas disponible');
@@ -6,11 +8,19 @@ export async function generatePDF(pdfMake, logoBase64, logoRenardBase64Gris) {
         return;
     }
 
+    pdfMake.vfs = {
+        'Courier.ttf': fontsBase64['Courier'],
+    };
+
     await waitForChart('investmentChart');
 
     const docDefinition = {
         pageSize: 'A4',
         pageMargins: [15, 15, 15, 50],
+        defaultStyle: {
+            font: 'Courier',
+             fontSize: 12
+         },
         background: function (currentPage, pageSize) {
             return {
                 image: logoRenardBase64Gris,
@@ -24,64 +34,36 @@ export async function generatePDF(pdfMake, logoBase64, logoRenardBase64Gris) {
             };
         },
         content: [
-            { text: 'Simulateur de Rendement d\'Investissement', style: 'title' },
-            { text: 'Informations sur l\'instrument financier', style: 'subtitle' },
+            { text: 'Simulateur de Rendement d\'Investissement' },
+            { text: 'Informations sur l\'instrument financier' },
             getStockInfo(),
-            { text: 'Synthèse investissement', style: 'subtitle' },
+            { text: 'Synthèse investissement' },
             getChartWithBorder('evolutionChart'),
             getTopResults(),
             {
-                text: 'Résultats', style: 'subtitle', pageBreak: 'before'
+                text: 'Résultats', pageBreak: 'before'
             },
             getResults(),
             {
-                text: 'Résultats avec écrêtage des gains', style: 'subtitle'
+                text: 'Résultats avec écrêtage des gains'
             },
             getResultsWithCapping(),
             getSecuredGainsTable(),
-            { text: 'Résultats épargne placée à taux garanti', style: 'subtitle' },
+            { text: 'Résultats épargne placée à taux garanti' },
             getResultsTauxFixe(),
-            { text: 'Graphiques évolutions des portefeuilles', style: 'subtitle', pageBreak: 'before' },
+            { text: 'Graphiques évolutions des portefeuilles', pageBreak: 'before' },
             getChartWithBorder('investmentChart'),
             getChartWithBorder('savingsChart'),
-            { text: 'Les performances passées des instruments financiers ne garantissent en aucun cas leurs performances futures. Ce simulateur est destiné à fournir une estimation basée sur des données historiques et ne prend pas en compte les événements imprévus, les évolutions du marché ou les frais associés aux investissements. Il est important de noter que les résultats obtenus ne constituent pas un conseil en investissement et que tout investissement comporte des risques, y compris la perte partielle ou totale du capital. Il est fortement recommandé de consulter un professionnel, tel qu\'un conseiller en gestion de patrimoine (CGP), avant de prendre toute décision d\'investissement, afin d\'obtenir des conseils personnalisés en fonction de votre profil et de vos objectifs financiers.', style: 'paragraph' },
+            { text: 'Les performances passées des instruments financiers ne garantissent en aucun cas leurs performances futures. Ce simulateur est destiné à fournir une estimation basée sur des données historiques et ne prend pas en compte les événements imprévus, les évolutions du marché ou les frais associés aux investissements. Il est important de noter que les résultats obtenus ne constituent pas un conseil en investissement et que tout investissement comporte des risques, y compris la perte partielle ou totale du capital. Il est fortement recommandé de consulter un professionnel, tel qu\'un conseiller en gestion de patrimoine (CGP), avant de prendre toute décision d\'investissement, afin d\'obtenir des conseils personnalisés en fonction de votre profil et de vos objectifs financiers.' },
         ],
-        styles: {
-            title: {
-                fontSize: 18,
-                bold: true,
-                alignment: 'center',
-                margin: [0, 0, 0, 15]
-            },
-            subtitle: {
-                fontSize: 14,
-                bold: true,
-                alignment: 'center',
-                margin: [0, 10, 10, 15]
-            },
-            paragraph: {
-                fontSize: 8,
-                alignment: 'justify',
-                margin: [10, 20, 10, 10]
-            },
-            tableHeader: {
-                bold: true,
-                fillColor: '#dddddd',
-                margin: [15, 5, 0, 5]
-            },
-            tableCell: {
-                margin: [0, 0, 0, 0]
-            },
-            positive: {
-                color: 'green'
-            },
-            negative: {
-                color: 'red'
-            },
-            chartContainer: {
-                margin: [0, 0, 0, 20],
-            }
-        },
+         fonts: {
+          'Courier': {
+           normal: 'Courier.ttf',
+             bold: 'Courier.ttf',
+             italics: 'Courier.ttf',
+             bolditalics: 'Courier.ttf'
+           }
+         },
         footer: function (currentPage, pageCount) {
             return {
                 table: {
@@ -92,7 +74,7 @@ export async function generatePDF(pdfMake, logoBase64, logoRenardBase64Gris) {
                                 text: 'Fox Velocity',
                                 alignment: 'center',
                                 fontSize: 8,
-                                margin: [0, 10, 0, 0]
+                                margin: [0, 10, 0, 0],
                             },
                             {
                                 image: logoBase64,
@@ -155,7 +137,7 @@ export async function generatePDF(pdfMake, logoBase64, logoRenardBase64Gris) {
                     [`Type : ${stockType}`],
                     [`Industrie : ${stockIndustry}`]
                 ],
-                widths: ['*']
+                widths: ['*'],
             },
             layout: 'noBorders',
             margin: [0, 0, 0, 10],
